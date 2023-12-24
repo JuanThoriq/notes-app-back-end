411bbf8045dae3a4bf16614d23e8d6567b416c7c
JuanThoriq <juanthoriq@gmail.com>
2023-12-25 02:48:23 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line linebreak-style
+// Memuat kode untuk membuat, mengonfigurasi,
+// dan menjalankan server HTTP menggunakan Hapi
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  // eslint-disable-next-line no-console
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
